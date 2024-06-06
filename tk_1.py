d975cf2100baa205b40627288b5cc3c652049873
vladyka130 <music319@gmail.com>
2024-06-07 02:22:41 +0300

    Initial commit

---
+++ tk_1.py
@@ -0,0 +1,11 @@
+from tkinter import *
+
+window = Tk()
+window.title('FIRST TKINTER_WINDOW')
+#window.minsize(400, 400)
+#window.maxsize(400, 400)
+window.geometry("500x500")
+window.resizable(False, False)  
+window["bg"] = '#2F4F4F'
+
+window.mainloop()
---
+++ tk_2.py
@@ -0,0 +1,41 @@
+import random
+from tkinter import *
+
+w = Tk()
+w["bg"] = '#2F4F4F'
+w.geometry("500x300")
+w.resizable(height=False, width=False)
+
+text = Label(w, text='-First Programm-', font='Verdana 14', fg='black', bg="#0F4F4F")
+text.pack(side="top")
+
+text2 = Label(w, text='ANY_TEXT', font='Verdana 14', fg='black', bg="#2F4F4F")
+#text2.place(x=195, y=135)
+
+def f1():
+    text2.configure(text="cliced_1")
+def f2():
+    text2.configure(text="cliced_2")
+def f3():
+    text2.configure(text="cliced_3")
+def reset():
+    text2.configure(text="ANY_TEXT")
+
+first_btn = Button(text="1", width=30, command=f1)
+first_btn.place(x=139, y=30)
+
+second_btn = Button(text="2", width=30, command=f2)
+second_btn.place(x=139, y=65)
+
+thred_btn = Button(text="3", width=30, command=f3)
+thred_btn.place(x=139, y=100)
+
+reset = Button(text="RESET", width=30, command=reset)
+reset.place(x=139, y=165)
+
+text2.place(x=200,y=130)
+
+
+w.mainloop()
+
+
